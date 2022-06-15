e73766cbaa942bec0cb506c241363c95d02a1199
DimitarGateshki <dimitar_gateshki@abv.bg>
2022-06-15 11:59:55 +0300

    Declared the variables and added script.js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//Create variables
+let firstNumber='';
+let secondNumber='';
+let currentOperation=null;
+let clearScreen=false;
+
+const numbers=document.querySelectorAll(".number");
+const operators=document.querySelectorAll(".operator")
+const equals=document.querySelector("#equals");
+const del=document.querySelector("#del");
+const point=document.querySelector("#point");
+const opperation=document.querySelector("#opperation");
+const result=document.querySelector("#result");
